7a79455429226e8f74cc18e8d46fa7b2145f2e04
Shivam Shinde <shivamshinde92722@gmail.com>
2023-12-10 18:37:31 +0530

    insertion sort implemented

---
+++ Sorting algorithms/insertionSort.py
@@ -0,0 +1,34 @@
+
+
+def insertion_sort(elements):
+
+    if len(elements) == 0:
+        print("No elements present in the provided array. So returning an empty array as output:")
+        return elements
+    elif len(elements) == 1:
+        return elements
+    else:
+        for i in range(1, len(elements)):
+            anchor = elements[i]
+            j = i - 1
+            while (j>=0 and elements[j] > anchor):
+                elements[j+1] = elements[j]
+                j -= 1
+            elements[j + 1] = anchor
+        return elements
+
+
+if __name__ == "__main__":
+
+    tests = [
+        [11, 9, 29, 7, 2, 15, 28], 
+        [3, 7, 9, 11],
+        [25, 22, 21, 10],
+        [29, 15, 28],
+        [],
+        [6]
+    ]
+
+    for array in tests:
+        sorted_elements = insertion_sort(array)
+        print(sorted_elements)
