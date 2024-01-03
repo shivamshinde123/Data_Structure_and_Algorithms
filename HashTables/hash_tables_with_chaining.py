4d681f0a5eeb52ed157ef38f509a8e54a7fd1e30
Shivam Shinde <shivamshinde92722@gmail.com>
2024-01-03 23:10:06 +0530

    hash table implementation with chaining completed

--- HashTables/hash_tables_with_chaining.py
+++ HashTables/hash_tables_with_chaining.py
@@ -61,6 +61,103 @@ class HashTable:
             self.table[index] = new_node
             self.size += 1
 
+    """
+    The 'search' method retrieves the value associated with a given key.
+    It first gets the index where key-value pair should be stored using _hash
+    method. It then search the linked list at that index for the key. If it
+    finds the key, it returns the associated value. If it doesn't find the key,
+    it raises a KeyError
+    """
+
+    def search(self, key):
+        index = self._hash(key)
+
+        current = self.table[index]
+        while current:
+            if current.key == key:
+                return current.value
+            current = current.next
+        raise KeyError(key)
     
 
- 
+    """
+    The 'remove' methods removes a key-value pair from the hash-table. It
+    first gets the index where the pair should be stored using '_hash' method.
+    It then searches the linked list at that index for the key. If it finds the key,
+    it removes the node from the list. If it doesn't find the key, it raises a 'keyError'.
+    """
+
+    def remove(self, key):
+        index = self._hash(key)
+
+        previous = None
+        current = self.table[index]
+
+        while current:
+            if current.key == key:
+                if previous:
+                    previous.next = current.next
+                else:
+                    self.table[index] = current.next
+                self.size -= 1
+                return 
+            previous = current
+            current = current.next
+
+        raise KeyError(key)
+    
+    """
+    __str__ method returns a string representation of the hash table
+    """
+
+    def __Str__(self):
+        elements = []
+
+        for i in range(self.capacity):
+            current = self.table[i]
+            while current:
+                elements.append((current.key,current.value))
+                current = current.next
+        return str(elements)
+    
+    """__len__ magic method"""
+    def __len__(self):
+        return self.size
+    
+    """__contains magic method"""
+    def __contains__(self,key):
+        try:
+            self.search(key)
+            return True
+        except KeyError:
+            return False
+        
+
+ # Driver code 
+if __name__ == '__main__': 
+  
+    # Create a hash table with 
+    # a capacity of 5 
+    ht = HashTable(5) 
+  
+    # Add some key-value pairs 
+    # to the hash table 
+    ht.insert("apple", 3) 
+    ht.insert("banana", 2) 
+    ht.insert("cherry", 5) 
+  
+    # Check if the hash table 
+    # contains a key 
+    print("apple" in ht)  # True 
+    print("durian" in ht)  # False 
+  
+    # Get the value for a key 
+    print(ht.search("banana"))  # 2 
+  
+    # Update the value for a key 
+    ht.insert("strawberry", 4) 
+    print(ht.search("banana"))  # 4 
+  
+    ht.remove("apple") 
+    # Check the size of the hash table 
+    print(len(ht))  # 3
